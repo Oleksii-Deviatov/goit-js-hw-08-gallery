09da0f3486a9a0be82bb08bf71a7d37d3d99f2cc
lewyk <lewyk@hotmail.com>
2020-11-21 00:23:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
-// Разбей задание на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-// Открытие модального окна по клику на элементе галереи.
-// Подмена значения атрибута src элемента img.lightbox__image.
-// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-// Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того,
-//     чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
-// Стартовые файлы
-// В папке src ты найдешь стартовые файлы проекта с базовой разметкой и готовыми стилями.
-// В файле gallery - items.js есть массив объектов содержащих информацию о изображениях:
-// маленькое изображение, оригинальное и описание.
-// Разметка элемента галереи
-// Ссылка на оригинальное изображение должна храниться в data - атрибуте source на элементе img,
-//     и указываться в href ссылки(это необходимо для доступности).
-
-{/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li> */}
-// Дополнительно
-// Следующий функционал не обязателен при сдаче задания, но будет хорошей практикой по работе с событиями.
-
-// Закрытие модального окна по клику на div.lightbox__overlay.
-// Закрытие модального окна по нажатию клавиши ESC.
-// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
-
 import "./gallery-items.js";
 import galleryItems from "./gallery-items.js"
-
 
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 const ref = {
     galleryContainer: document.querySelector('.js-gallery'),
+    galleryImage: document.querySelector('.gallery__image'),
+    galleryLink: document.querySelector('.gallery__link'),
+    jsLightbox: document.querySelector('.js-lightbox'),
+    lightboxImage: document.querySelector('.lightbox__image'),
+    lightboxButton: document.querySelector('[data-action="close-lightbox"]'),
+    lightboxOverlay: document.querySelector('.lightbox__overlay'),
 }
 
 const galleryMarkup = createGalery(galleryItems);
 ref.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function createGalery(gallery) {
-    return gallery.map(({preview , original, description}) => {
+    return gallery.map(({preview , original, description},index) => {
         return `
         <li class="gallery__item">
             <a
@@ -58,6 +26,7 @@ function createGalery(gallery) {
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
+            data-index="${index}"
             alt="${description}"
             />
             </a>
@@ -65,4 +34,65 @@ function createGalery(gallery) {
         `;
     })
     .join('');
+}
+
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+ref.galleryContainer.addEventListener('click', openJsLightbox);
+
+function openJsLightbox(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const bigURL = e.target.dataset.source;
+    const imgALT = e.target.alt;
+    const index = e.target.dataset.index;
+
+    ref.jsLightbox.classList.add("is-open");
+    ref.lightboxImage.src = bigURL;
+    ref.lightboxImage.alt = imgALT;
+    ref.lightboxImage.dataset.index = index;
+}
+
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+
+ref.lightboxButton.addEventListener('click', closeJsLightbox);
+
+function closeJsLightbox() {
+    ref.jsLightbox.classList.remove("is-open");
+    ref.lightboxImage.src = '';
+    ref.lightboxImage.alt = '';
+}
+
+// Закрытие модального окна по клику на div.lightbox__overlay.
+ref.lightboxOverlay.addEventListener('click', closeJsLightbox);
+
+// Закрытие модального окна по нажатию клавиши ESC.
+
+document.addEventListener('keydown', closeJsLightboxByKey);
+
+function closeJsLightboxByKey(e) {
+    if (e.code  === "Escape") {
+        closeJsLightbox();
+    }
+}
+
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+
+document.addEventListener('keydown', (e) => scrollGallery(e, galleryItems));
+
+function scrollGallery(e, gallery) {
+    let index = parseInt(ref.lightboxImage.dataset.index);
+    if (e.code === "ArrowLeft" && index > 0) {
+        index -= 1;
+        ref.lightboxImage.dataset.index = index;
+        ref.lightboxImage.src = gallery[index].original;
+    }
+        if (e.code === "ArrowRight" && index < gallery.length - 1) { 
+            index += 1;
+            ref.lightboxImage.dataset.index = index;
+            ref.lightboxImage.src = gallery[index].original;
+    }
 }
